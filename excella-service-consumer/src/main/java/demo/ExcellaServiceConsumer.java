c95fd015d7e44638a3971beb3f42149341a85397
Tekle Ayele <tekleayele@gmail.com>
2018-09-24 16:09:03 -0400

    Upated URL paht

--- excella-service-consumer/src/main/java/demo/ExcellaServiceConsumer.java
+++ excella-service-consumer/src/main/java/demo/ExcellaServiceConsumer.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -16,7 +17,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(exclude = RabbitAutoConfiguration.class)
+@SpringBootApplication(exclude = {RabbitAutoConfiguration.class, SecurityAutoConfiguration.class})
 @EnableEurekaClient
 @EnableCircuitBreaker
 public class ExcellaServiceConsumer {
