41932e75eb598de169d57438427650878fc802dc
Tekle Ayele <tekleayele@gmail.com>
2018-09-27 13:09:50 -0400

    Added more docker files

--- excella-eureka-server/src/main/java/demo/EurekaServerApplication.java
+++ excella-eureka-server/src/main/java/demo/EurekaServerApplication.java
@@ -1,6 +1,7 @@
 package demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
