8e449363cb3009f47b3a5da6590500f786464ac2
Tekle Ayele <tekleayele@gmail.com>
2018-09-27 14:26:12 -0400

    Updated yml files

--- excella-config-server/src/main/java/demo/ExcellaConfigServer.java
+++ excella-config-server/src/main/java/demo/ExcellaConfigServer.java
@@ -2,12 +2,13 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 
-@SpringBootApplication
+@SpringBootApplication (exclude = { SecurityAutoConfiguration.class })
 @EnableConfigServer
 public class ExcellaConfigServer {
 
